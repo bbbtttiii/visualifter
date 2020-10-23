4e09ab6e11c0210d2a281cd8e43fe3155551934f
bbbtttiii <ben.iburg@gmail.com>
2020-10-23 17:42:47 -0500

    add blocks controller methods

--- backend/app/controllers/blocks_controller.rb
+++ backend/app/controllers/blocks_controller.rb
@@ -1,13 +1,34 @@
 class BlocksController < ApplicationController
 
     def index
-        
+        blocks = Block.all
+        render json: blocks
+    end
+
+    def create
+        block = Block.find(params[:id])
+        render json: block
+    end
+
+    def update
+        block = Block.find(params[:id]) 
+        block.update
+        if block.save
+            render json: block
+        else
+            #render error
+        end
+    end
+
+    def destroy
+        block = Block.find(params[:id])
+        block.destroy
     end
 
     private
 
     def block_params
-        params.require(:block).permit(:exercise, :weight, :reps, :sets)
+        params.require(:block).permit(:exercise, :weight, :reps, :sets, :workout_id)
     end
 
 end
