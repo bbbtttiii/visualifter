47bd668c58392705b35757d1978db8df286c634a
bbbtttiii <ben.iburg@gmail.com>
2020-10-26 10:19:18 -0500

    css fixes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,16 +1,16 @@
-class Api {
-    constructor() {
-        this.baseUrl = `https://localhost:3000`;
-    }
+// class Api {
+//     constructor() {
+//         this.baseUrl = `https://localhost:3000`;
+//     }
 
-    // update(id, body) {
-    //     return fetch(`${this.baseUrl}/${id}`, {
-    //       method: 'PATCH',
-    //       headers: {
-    //         'Content-Type': 'application/json',
-    //         Accept: 'application/json'
-    //       },
-    //       body: JSON.stringify(body),
-    //     }).then(res=>res.json());
-    //   }
-}
+//     // update(id, body) {
+//     //     return fetch(`${this.baseUrl}/${id}`, {
+//     //       method: 'PATCH',
+//     //       headers: {
+//     //         'Content-Type': 'application/json',
+//     //         Accept: 'application/json'
+//     //       },
+//     //       body: JSON.stringify(body),
+//     //     }).then(res=>res.json());
+//     //   }
+// }
