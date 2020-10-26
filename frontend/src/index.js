df884bec1bfea6bd5da8233b2576c8713b1d97d6
bbbtttiii <ben.iburg@gmail.com>
2020-10-26 10:47:12 -0500

    bring back block function + dragging

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,64 +1,67 @@
-
-
 function makeBlock(reps, sets, weight) {
-    const container = document.getElementById("container")
+    const container = document.getElementsByClassName('main')[0];
     let block = document.createElement('div');
     block.className = 'block';
-    container.append(block);
+    block.style.setProperty('--grid-rows', reps);
+    block.style.setProperty('--grid-cols', sets);
+    for (c = 0; c < (reps * sets); c++) {
+      let cell = document.createElement('div');
+      cell.innerText = weight;
+      container.appendChild(block)
+      block.appendChild(cell).className = 'grid-item';
+    }
 }
 
 // function makeBlock(reps, sets, weight) {
-//   block.style.setProperty('--grid-rows', reps);
-//   block.style.setProperty('--grid-cols', sets);
-//   for (c = 0; c < (reps * sets); c++) {
-//     let cell = document.createElement("div");
-//     cell.innerText = weight;
-//     container.appendChild(block)
-//     block.appendChild(cell).className = "grid-item";
+  // block.style.setProperty('--grid-rows', reps);
+  // block.style.setProperty('--grid-cols', sets);
+  // for (c = 0; c < (reps * sets); c++) {
+  //   let cell = document.createElement("div");
+  //   cell.innerText = weight;
+  //   container.appendChild(block)
+  //   block.appendChild(cell).className = "grid-item";
 //   }
 // }
 
 makeBlock(5, 5, 185);
-
 
 //set, rep, weight//
 
 
 
 
-dragElement(document.querySelectorAll(".block"));
+var draggableElements = document.getElementsByClassName('block');
+
+for(var i = 0; i < draggableElements.length; i++){
+    dragElement(draggableElements[i]);
+}
 
 function dragElement(elmnt) {
-  let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  elmnt.onmousedown = dragMouseDown;
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    elmnt.onmousedown = dragMouseDown;
+    
+    function dragMouseDown(e) {
+        e = e || window.event;
+        pos3 = parseInt(e.clientX);
+        pos4 = parseInt(e.clientY);
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+        return false;
+    }
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.x;
-    pos4 = e.y;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
+    function elementDrag(e) {
+        e = e || window.event;
+        pos1 = pos3 - parseInt(e.clientX);
+        pos2 = pos4 - parseInt(e.clientY);
+        pos3 = parseInt(e.clientX);
+        pos4 = parseInt(e.clientY);
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        console.log(elmnt.offsetTop)
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.x;
-    pos2 = pos4 - e.y;
-    pos3 = e.x;
-    pos4 = e.y;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
 }
