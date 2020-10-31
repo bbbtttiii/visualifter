d9d4320b30b19630764299decb8057f25ae0599f
bbbtttiii <ben.iburg@gmail.com>
2020-10-30 23:09:11 -0500

    removes unused controller methods

--- backend/app/controllers/workouts_controller.rb
+++ backend/app/controllers/workouts_controller.rb
@@ -12,29 +12,6 @@ class WorkoutsController < ApplicationController
         end
     end
 
-    def show
-        workout = Workout.find(params[:id])
-        render json: workout, include: ['blocks']
-    end
-
-    def blocks
-        block = Block.find(params[:id])
-        render json: workout.blocks
-    end
-
-    def update 
-        workout = Workout.find(params[:id])
-        workout.update(workout_params)
-        if workout.save
-            render json: workout
-        end
-    end
-
-    def destroy
-        workout = Workout.find(params[:id])
-        workout.destroy
-    end
-
     private
 
     def workout_params
