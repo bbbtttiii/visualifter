ecaa1007a23694d98128cf14490898f069a1bdba
bbbtttiii <ben.iburg@gmail.com>
2021-02-03 15:16:08 -0600

    heroku backend update

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_10_27_210127) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blocks", force: :cascade do |t|
     t.string "exercise"
     t.integer "reps"
