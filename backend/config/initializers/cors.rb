6d7faf318fc583d550d5a7b6c5fd98f27453e0bb
bbbtttiii <ben.iburg@gmail.com>
2021-03-12 20:45:15 -0600

    update cors url

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://visualifter.netlify.com/frontend'
+    origins 'https://visualifter.netlify.com'
 
     resource '*',
       headers: :any,
