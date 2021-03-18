e6d99bf2052a3f7920bb09232ccb15456dea7bc4
bbbtttiii <ben.iburg@gmail.com>
2021-03-18 14:18:52 -0500

    fix url

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://visualifter.netlify.com'
+    origins 'https://visualifter.netlify.app'
 
     resource '*',
       headers: :any,
